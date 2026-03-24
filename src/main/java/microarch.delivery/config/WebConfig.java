c1640881de5890f8bb2a904ac32a0970e1340d8a
ruakgb <kabilan.kumaraguru@raiffeisen.ru>
2026-03-24 16:21:35 +0300

    module-8: Added api

--- src/main/java/microarch.delivery/config/WebConfig.java
+++ src/main/java/microarch.delivery/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8082")
+                .allowedOrigins("http://localhost:8086")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
